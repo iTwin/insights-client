ed6dd6f11387500a01e74ea6042d40a55485bd9d
helenzhou0523 <helen.zhou@bentley.com>
2022-02-04 14:19:47 -0800

    initial commit with all api and client definitions

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# insights-client
+# Insights Client Library
+
+Copyright © Bentley Systems, Incorporated. All rights reserved. See [LICENSE.md](./LICENSE.md) for license terms and full copyright notice.
+
+[iTwin.js](http://www.itwinjs.org) is an open source platform for creating, querying, modifying, and displaying Infrastructure Digital Twins. To learn more about the iTwin Platform and its APIs, visit the [iTwin developer portal](https://developer.bentley.com/).
+
+If you have questions, or wish to contribute to iTwin.js, see our [Contributing guide](./CONTRIBUTING.md).
+
+## About this Repository
+
+Contains the __@itwin/insights-client__ package that wraps sending requests to the project service. Visit the [Insights API](https://developer.bentley.com/apis/insights/) and [Carbon calculation API](https://developer.bentley.com/apis/carbon-calculation/) for more documentation on the insights service.
+
+## Environment Variables
+
+```
+# ---- Optional URL prefix for dev/qa environments ----
+IMJS_URL_PREFIX=""
+```
---
+++ src/oneClickLCA/oneClickLCAClient.ts
@@ -0,0 +1,58 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+import { AccessToken } from "@itwin/core-bentley";
+import { OneClickLCAApi, BASE_PATH } from "./generated";
+import { JobCreateCarbonCalculationAPI } from "./generated/api";
+
+const ACCEPT = "application/vnd.bentley.itwin-platform.v1+json";
+
+const prefixUrl = (baseUrl?: string, prefix?: string) => {
+  if (prefix && baseUrl) {
+    return baseUrl.replace("api.bentley.com", `${prefix}api.bentley.com`);
+  }
+  return baseUrl;
+};
+
+export class OneClickLCAClient {
+  private _oclcaApi: OneClickLCAApi;
+  constructor() {
+    const baseUrl = prefixUrl(BASE_PATH, process.env.IMJS_URL_PREFIX);
+    this._oclcaApi = new OneClickLCAApi(undefined, baseUrl); // TODO: change to baseUrl in deployment
+  }
+
+  public async getOneclicklcaAccessToken(username: string, password: string) {
+    if (username === undefined || password === undefined) {
+      return undefined;
+    }
+
+    let response = await fetch("https://oneclicklcaapp.com/app/api/login", {
+      method: "POST",
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    });
+
+    if (response.ok) {
+      return await response.json();
+    } else {
+      return undefined;
+    }
+  }
+
+  public async createOneclicklcaJob(
+    accessToken: AccessToken,
+    job: JobCreateCarbonCalculationAPI
+  ) {
+    return this._oclcaApi.createOneclicklcaJob(accessToken, job, ACCEPT);
+  }
+
+  public async getOneclicklcaJobStatus(
+    accessToken: AccessToken,
+    jobId: string
+  ) {
+    return this._oclcaApi.getOneclicklcaJobStatus(jobId, accessToken, ACCEPT);
+  }
+}
