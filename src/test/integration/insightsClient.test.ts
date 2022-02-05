334af16965f689dde1c447e444427166547c67fc
helenzhou0523 <helen.zhou@bentley.com>
2022-02-04 16:10:14 -0800

    add test setup

---
+++ src/test/integration/insightsClient.test.ts
@@ -0,0 +1,28 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+import * as chai from "chai";
+import type { AccessToken } from "@itwin/core-bentley";
+import { TestConfig } from "../TestConfig";
+import type { Project } from "@itwin/projects-client";
+import { ProjectsAccessClient } from "@itwin/projects-client";
+
+chai.should();
+describe("ProjectsClient", () => {
+  let accessToken: AccessToken;
+
+  before(async function () {
+    this.timeout(0);
+    accessToken = await TestConfig.getAccessToken();
+  });
+
+  it("should get a list of projects", async () => {
+    const projectsAccessClient = new ProjectsAccessClient();
+    const projectList: Project[] = await projectsAccessClient.getAll(accessToken);
+    if(projectList){
+      console.log(projectList.length);
+      console.log(projectList[0].name);
+    }
+  });
+});
