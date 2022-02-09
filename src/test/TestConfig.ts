8d4fb9ed11075d48d62648a61d0b6dc8aa03717a
Helen Zhou <helen.zhou@bentley.com>
2022-02-09 10:33:39 -0800

    add tests

--- src/test/TestConfig.ts
+++ src/test/TestConfig.ts
@@ -12,9 +12,6 @@ import {
 /** Basic configuration used by all tests
  */
 export class TestConfig {
-  /** Name of the project used by some tests */
-  public static readonly projectName: string = "iModelJsIntegrationTest";
-
   /** Login the specified user and return the AuthorizationToken */
   public static async getAccessToken(
     user: TestUserCredentials = TestUsers.regular
--- src/test/integration/insightsClient.test.ts
+++ src/test/integration/insightsClient.test.ts
@@ -5,21 +5,58 @@
 import * as chai from "chai";
 import type { AccessToken } from "@itwin/core-bentley";
 import { TestConfig } from "../TestConfig";
-import type { Project } from "@itwin/projects-client";
-import { ProjectsAccessClient } from "@itwin/projects-client";
+import { expect } from 'chai';
+import { MappingReportingAPI } from './../../../lib/cjs/reporting/generated/api.d';
+import { ReportCollectionReportingAPI, MappingCollectionReportingAPI } from './../../../lib/esm/reporting/generated/api.d';
+import { ReportingClient } from './../../reporting/reportingClient';
 
 chai.should();
-describe("ProjectsClient", () => {
+describe("ReportingClient", () => {
+  const projectId = "8b4b17c9-2a99-4e83-9637-c587bef27e30";
+  const iModelId = "8d73d54f-a340-4fad-b579-07403541cd99";
+
   let accessToken: AccessToken;
+  let reportingCLient: ReportingClient;
+  let reportId: string | undefined;
+  let mappingId: string | undefined;
+  let groupId: string | undefined;
+
 
   before(async function () {
     this.timeout(0);
     accessToken = await TestConfig.getAccessToken();
   });
 
-  it("should get a list of projects", async () => {
-    const projectsAccessClient = new ProjectsAccessClient();
-    const projectList: Project[] = await projectsAccessClient.getAll(accessToken);
-    chai.expect(projectList).to.be.not.empty;
+  it("should get a list of reports", async () => {
+    const reportList = await reportingCLient.getReports(accessToken, projectId);
+    expect(reportList.reports).to.be.not.empty;
+    if(reportList?.reports && reportList.reports.length > 0) {
+      reportId = reportList.reports[0].id;
+    }
+  });
+
+  it("should get a list of mappings", async () => {
+    const mappingList = await reportingCLient.getMappings(accessToken, iModelId);
+    expect(mappingList).to.be.not.empty;
+    if(mappingList && mappingList.length > 0) {
+      mappingId = mappingList[0].id;
+    }
+  });
+
+  it("should get a list of groups", async () => {
+    if(mappingId) {
+      const groups = await reportingCLient.getGroups(accessToken, iModelId, mappingId);
+      expect(groups.groups).to.be.not.empty;
+      if(groups?.groups && groups.groups.length > 0) {
+        groupId = groups.groups[0].id;
+      }
+    }
+  });
+
+  it("should get a list of groupProperties", async () => {
+    if(mappingId && groupId) {
+      const properties = await reportingCLient.getGroupProperties(accessToken, iModelId, mappingId, groupId);
+      expect(properties.properties).to.be.not.empty;
+    }
   });
 });
