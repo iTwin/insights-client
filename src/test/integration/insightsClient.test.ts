be033ec6c2e4aee6f0bec8ce39c0003ba7d0832d
RytisSapka <107986502+RytisSapka@users.noreply.github.com>
2022-07-10 13:58:37 +0300

    test coverage config

--- src/test/integration/insightsClient.test.ts
+++ src/test/integration/insightsClient.test.ts
@@ -6,36 +6,39 @@ import * as chai from "chai";
 import { expect } from "chai";
 import type { AccessToken } from "@itwin/core-bentley";
 import { TestConfig } from "../TestConfig";
-import { ReportingClient } from "./../../reporting/reportingClient";
+import { ExtractionClient, MappingsClient, ODataClient, ReportsClient } from "./../../reporting";
 
 chai.should();
 describe("ReportingClient", () => {
-  const reportingClient: ReportingClient = new ReportingClient();
+  const extractionClient: ExtractionClient = new ExtractionClient();
+  const mappingsClient: MappingsClient = new MappingsClient();
+  const oDataClient: ODataClient = new ODataClient();
+  const reportsClient: ReportsClient = new ReportsClient();
   const projectId = process.env.IMJS_TEST_PROJECT_ID ?? "";
   const imodelId = process.env.IMJS_TEST_IMODEL_ID ?? "";
   const mappingId = process.env.IMJS_TEST_MAPPING_ID ?? "";
   const groupId = process.env.IMJS_TEST_GROUP_ID ?? "";
-  let accessToken: AccessToken;
+  let accessToken = process.env.IMJS_TEST_AUTH_ID ?? "";
 
   before(async function () {
     this.timeout(0);
-    accessToken = await TestConfig.getAccessToken();
+    //accessToken = await TestConfig.getAccessToken();
   });
 
   it("Get reportis", async () => {
-    const reports = await reportingClient.getReports(accessToken, projectId);
+    const reports = await reportsClient.getReports(accessToken, projectId);
     expect(reports).to.not.be.undefined;
     expect(reports).to.not.be.empty;
   });
 
   it("Get mappings", async () => {
-    const mappings = await reportingClient.getMappings(accessToken, imodelId);
+    const mappings = await mappingsClient.getMappings(accessToken, imodelId);
     expect(mappings).to.not.be.undefined;
     expect(mappings).to.not.be.empty;
   });
 
   it("Get groups", async () => {
-    const groups = await reportingClient.getGroups(
+    const groups = await mappingsClient.getGroups(
       accessToken,
       imodelId,
       mappingId
@@ -45,7 +48,7 @@ describe("ReportingClient", () => {
   });
 
   it("Get properties", async () => {
-    const properties = await reportingClient.getGroupProperties(
+    const properties = await mappingsClient.getGroupProperties(
       accessToken,
       imodelId,
       mappingId,
