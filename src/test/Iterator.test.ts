7fa08befa1ad59a959ddee406c91f89dbf592984
RytisSapka <107986502+RytisSapka@users.noreply.github.com>
2022-07-25 17:04:32 +0300

    lint fix

--- src/test/Iterator.test.ts
+++ src/test/Iterator.test.ts
@@ -4,8 +4,6 @@
 *--------------------------------------------------------------------------------------------*/
 import * as chaiAsPromised from "chai-as-promised"
 import { expect, use } from "chai";
-import * as sinon from "sinon";
-import { MappingsClient, Mapping, MappingCreate, MappingUpdate, Group, GroupCreate, GroupUpdate, GroupProperty, GroupPropertyCreate, DataType, QuantityType, ECProperty, CalculatedProperty, CalculatedPropertyCreate, CalculatedPropertyType, CustomCalculation, CustomCalculationCreate, MappingCopy } from "../reporting";
 import { EntityListIteratorImpl } from "../reporting/iterators/EntityListIteratorImpl";
 import { toArray } from "../reporting/iterators/IteratorUtil"
 use(chaiAsPromised);
@@ -15,12 +13,10 @@ describe("mappings Client", () => {
   it("toArray", async () => {
     const it = new EntityListIteratorImpl(async () => {
       return {
-        entities: new Array(0, 1, 2),
+        entities: [0, 1, 2],
         next: undefined
       }
     });
     expect((await toArray(it)).length).to.be.eq(3);
   });
-
-  
 });
