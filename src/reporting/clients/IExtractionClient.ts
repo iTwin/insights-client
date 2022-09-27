ffe12f2bf126153afa216e4c3c6863b257048a82
RytisSapka <107986502+RytisSapka@users.noreply.github.com>
2022-09-27 16:54:27 +0300

    added get history endpoint (#18)
    * added get history endpoint
    
    * fixes
    
    * version bump
    
    * package lock bump
    
    Co-authored-by: MindaugasButkus <87982008+MindaugasButkus@users.noreply.github.com>

--- src/reporting/clients/IExtractionClient.ts
+++ src/reporting/clients/IExtractionClient.ts
@@ -3,7 +3,7 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 import type { AccessToken } from "@itwin/core-bentley";
-import type { ExtractionLog, ExtractionRun, ExtractionStatus } from "../interfaces/ExtractionProcess";
+import type { Extraction, ExtractionLog, ExtractionRun, ExtractionStatus } from "../interfaces/ExtractionProcess";
 import type { EntityListIterator } from "../iterators/EntityListIterator";
 
 export interface IExtractionClient {
@@ -59,4 +59,33 @@ export interface IExtractionClient {
     accessToken: AccessToken,
     jobId: string
   ): Promise<ExtractionStatus>;
+
+  /**
+   * Gets Extractions of an IModel. This method returns the full list of Extractions.
+   * @param {string} iModelId The iModel Id.
+   * @param {string} accessToken OAuth access token with scope `insights:read`.
+   * @param {number} top The number of entities to load per page.
+   * @memberof ReportingClient
+   * @link https://developer.bentley.com/apis/insights/operations/get-extraction-history/
+   */
+  getExtractionHistory(
+    accessToken: AccessToken,
+    iModelId: string,
+    top?: number
+  ): Promise<Extraction[]>;
+
+  /**
+   * Gets an async paged iterator of Extractions for an IModel.
+   * This method returns an iterator which loads pages of extractions as it is being iterated over.
+   * @param {string} iModelId The iModel Id.
+   * @param {string} accessToken OAuth access token with scope `insights:read`.
+   * @param {number} top The number of entities to load per page.
+   * @memberof ReportingClient
+   * @link https://developer.bentley.com/apis/insights/operations/get-extraction-history/
+   */
+  getExtractionHistoryIterator(
+    accessToken: AccessToken,
+    iModelId: string,
+    top?: number
+  ): EntityListIterator<Extraction>;
 }
--- src/test/ExtractionClient.test.ts
+++ src/test/ExtractionClient.test.ts
@@ -5,7 +5,7 @@
 import * as chaiAsPromised from "chai-as-promised";
 import { expect, use } from "chai";
 import * as sinon from "sinon";
-import { ExtractionClient, ExtractionLog, ExtractorState } from "../reporting";
+import { Extraction, ExtractionClient, ExtractionLog, ExtractorState } from "../reporting";
 use(chaiAsPromised);
 
 describe("Extraction Client", () => {
@@ -138,6 +138,80 @@ describe("Extraction Client", () => {
     extraction = await extractionClientNewBase.getExtractionStatus("auth", "jobId");
     expect(fetchStub.calledWith(
       "BASE/datasources/extraction/status/jobId",
+      "pass",
+    )).to.be.true;
+  });
+
+  it("Get History", async () => {
+    const returns1 = {
+      extractions: [1, 2],
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      _links: {
+        next: {
+          href: "url",
+        },
+      },
+    };
+    const returns2 = {
+      extractions: [3, 4],
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      _links: {
+        next: undefined,
+      },
+    };
+    fetchStub.withArgs("https://api.bentley.com/insights/reporting/datasources/imodels/iModelId/extraction/history", "pass").resolves(returns1)
+      .withArgs("url", "pass").resolves(returns2)
+      .withArgs("BASE/datasources/imodels/iModelId/extraction/history", "pass").resolves(returns2);
+
+    let extraction: Array<Extraction> = await extractionClient.getExtractionHistory("auth", "iModelId");
+    expect(extraction.length).to.be.eq(4);
+    expect(extraction[0]).to.be.eq(1);
+    expect(extraction[3]).to.be.eq(4);
+    expect(fetchStub.calledWith(
+      "https://api.bentley.com/insights/reporting/datasources/imodels/iModelId/extraction/history",
+      "pass",
+    )).to.be.true;
+
+    extraction = await extractionClientNewBase.getExtractionHistory("auth", "iModelId");
+    expect(fetchStub.calledWith(
+      "BASE/datasources/imodels/iModelId/extraction/history",
+      "pass",
+    )).to.be.true;
+  });
+
+  it("Get Logs with top", async () => {
+    const returns1 = {
+      extractions: [1, 2],
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      _links: {
+        next: {
+          href: "url",
+        },
+      },
+    };
+    const returns2 = {
+      extractions: [3, 4],
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      _links: {
+        next: undefined,
+      },
+    };
+    fetchStub.withArgs("https://api.bentley.com/insights/reporting/datasources/imodels/iModelId/extraction/history/?$top=2", "pass").resolves(returns1)
+      .withArgs("url", "pass").resolves(returns2)
+      .withArgs("BASE/datasources/imodels/iModelId/extraction/history/?$top=2", "pass").resolves(returns2);
+
+    let extraction: Array<Extraction> = await extractionClient.getExtractionHistory("auth", "iModelId", 2);
+    expect(extraction.length).to.be.eq(4);
+    expect(extraction[0]).to.be.eq(1);
+    expect(extraction[3]).to.be.eq(4);
+    expect(fetchStub.calledWith(
+      "https://api.bentley.com/insights/reporting/datasources/imodels/iModelId/extraction/history/?$top=2",
+      "pass",
+    )).to.be.true;
+
+    extraction = await extractionClientNewBase.getExtractionHistory("auth", "iModelId", 2);
+    expect(fetchStub.calledWith(
+      "BASE/datasources/imodels/iModelId/extraction/history/?$top=2",
       "pass",
     )).to.be.true;
   });
