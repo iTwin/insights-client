c49175ddfb89d9b1587362af5222a836710b049e
Guillar1 <Guillar1@users.noreply.github.com>
2023-03-02 20:34:19 -0800

    Update IOCLCAJobsClient.ts

--- src/carbon-calculation/clients/IOCLCAJobsClient.ts
+++ src/carbon-calculation/clients/IOCLCAJobsClient.ts
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
 import { AccessToken } from "@itwin/core-bentley";
 import { OCLCAJob, OCLCAJobCreate, OCLCAJobStatus } from "../interfaces/OCLCAJobs";
 
