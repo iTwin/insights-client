f0ec6ac853406c21842dd60621f733f1da23729b
Julio Alva <julio.alva@bentley.com>
2024-03-14 08:07:44 -0400

    cdm client integration test

--- src/integration-test/integration/grouping-and-mapping/CDMClient.test.ts
+++ src/integration-test/integration/grouping-and-mapping/CDMClient.test.ts
@@ -2,7 +2,40 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
+import { expect } from "chai";
+import { ExtractionStatus, Mapping } from "../../../grouping-and-mapping";
+import { accessToken, cdmClient, extractionClient, mappingsClient, testIModel } from "../../utils";
 
-describe("CDM Client Integration Tests", ()=> {
-  // TODO: Need extraction client.
+describe.only("CDM Client Integration Tests", ()=> {
+  let mappingOne: Mapping;
+  let extraction: ExtractionStatus;
+
+  before(async () => {
+    mappingOne = await mappingsClient.createMapping(accessToken, {
+      iModelId: testIModel.id,
+      mappingName: "mappingOne",
+    });
+
+    extraction = await extractionClient.runExtraction(accessToken, {
+      iModelId: testIModel.id,
+      mappings: [
+        { id: mappingOne.id },
+      ],
+    });
+  });
+
+  after(async () => {
+    await mappingsClient.deleteMapping(accessToken, mappingOne.id);
+  });
+
+  it("CDM Client - Get CDM and Get CDM partition", async ()=> {
+    const cdm = await cdmClient.getCDM(accessToken, mappingOne.id, extraction.id);
+    expect(cdm).not.be.undefined;
+
+    const entity = cdm.entities[0];
+    const cdmPartitionLocation = entity.partitions[0].location;
+    const cdmPartition = await cdmClient.getCDMPartition(accessToken, mappingOne.id, extraction.id, cdmPartitionLocation);
+    expect(cdmPartition.status).to.be.equal(200);
+  });
+
 });
